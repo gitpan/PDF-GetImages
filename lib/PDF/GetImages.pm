dd7ec0ca93605195b8e041ed9c914a1c37153000
Leo Charre <leocharre@cpan.org>
2009-01-13 14:51:35 +0000

    Import of LEOCHARRE/PDF-GetImages-1.17 from CPAN.
    gitpan-cpan-distribution: PDF-GetImages
    gitpan-cpan-version:      1.17
    gitpan-cpan-path:         LEOCHARRE/PDF-GetImages-1.17.tar.gz
    gitpan-cpan-author:       LEOCHARRE
    gitpan-cpan-maturity:     released

--- lib/PDF/GetImages.pm
+++ lib/PDF/GetImages.pm
@@ -3,98 +3,119 @@ use strict;
 use File::Which 'which';
 use Carp;
 require Exporter;
-use vars qw(@EXPORT_OK @ISA $WHICH_CONVERT $WHICH_PDFIMAGES $VERSION $DEBUG $errstr);
+use Cwd;
+use vars qw(@EXPORT_OK @ISA $WHICH_CONVERT $WHICH_PDFIMAGES $VERSION $DEBUG $errstr $FORCE_JPG @TRASH);
 @ISA = qw(Exporter);
 @EXPORT_OK = qw(pdfimages);
-$VERSION = sprintf "%d.%02d", q$Revision: 1.13 $ =~ /(\d+)/g;
-$PDF::GetImages::FORCE_JPG=0;
-
-$WHICH_CONVERT = which('convert');
-$WHICH_PDFIMAGES = which('pdfimages')
+$VERSION = sprintf "%d.%02d", q$Revision: 1.17 $ =~ /(\d+)/g;
+$FORCE_JPG=0;
+$WHICH_CONVERT ||= which('convert');
+$WHICH_PDFIMAGES ||= which('pdfimages')
    or croak( " is pdfimages (xpdf) installed? Cant get which() pdfimages");
 
-sub errstr {
-   $errstr = $_[0] if defined $_[0];
-   return $errstr;
-}
+sub errstr { $errstr = $_[0] if defined $_[0]; 1 }
 
-sub debug {
-   my $m = shift; 
-   $m||=''; 
-   $DEBUG or return 1; 
-   print STDERR " # PDF::GetImages # $m\n"; 
-   return 1; 
-}
+sub debug { $DEBUG and print STDERR __PACKAGE__.", @_\n"; 1 }
+
 
 
 sub pdfimages {
 	my ($_abs_pdf,$_dir_out) = @_;
    defined $_abs_pdf or croak('missing argument');
 
-   require Cwd;
+   no warnings;
+   debug("args: in '$_abs_pdf'");
+
+   carp($_abs_pdf) if $DEBUG;
+
    my $cwd = Cwd::cwd();
 
    my $abs_pdf = Cwd::abs_path($_abs_pdf)
-      or croak("can't resolve location of $_abs_pdf");
+      or errstr("can't resolve location of '$_abs_pdf', cwd is $cwd")
+      and return;
    
-   -f $abs_pdf or carp("$abs_pdf not on disk.") and return [];
+   -f $abs_pdf or errstr("ERROR: $abs_pdf is NOT on disk.") and return;
 
 
    $abs_pdf=~/(.+)\/([^\/]+)(\.pdf)$/i
-      or carp("$abs_pdf not '.pdf'?")
-      and return [];
+      or errstr("$abs_pdf not '.pdf'?")
+      and return;
 
 
    my ($abs_loc,$filename,$filename_only) = ($1,"$2$3",$2);
    
    my $_copied=0;
-   if( $_dir_out ){
-      my $dir_out = Cwd::abs_path($_dir_out) or croak("cant resolve $_dir_out");
+   if( $_dir_out ){ # did user specify a dir out to
+      debug("have dir out arg '$_dir_out'.. ");
+      my $dir_out = Cwd::abs_path($_dir_out) 
+         or croak("cant resolve $_dir_out, should be able to, please notify PDF::GetImages AUTHOR");
+      debug("have dir out '$_dir_out', resolved to $dir_out");
 
       if ($dir_out ne $abs_loc){
+         debug("dir out not same as original file loc");
           -d $dir_out or croak("Dir out arg is not a dir $dir_out");
 
          require File::Copy;
          File::Copy::copy($abs_pdf,"$dir_out/$filename") 
-            or croak("you specified dir out $dir_out, but we cant copy $abs_pdf there, $!");
-         $_copied=1;
+            or croak("you specified dir out $dir_out, but we cant copy '$abs_pdf' there, $!");
          $abs_loc=$dir_out;
          $abs_pdf = "$dir_out/$filename";
+         push @TRASH, $abs_pdf;
          debug("switched to use pdf copy $abs_pdf");
       }
    }
 
-	
+	#debug("changing dir to abs loc '$abs_loc'");
+   # WHY chdir??? I think this causes problems to sub scripts etc... ????
 
-	chdir($abs_loc) 
-      or carp("pdfimages() cannot chdir into $abs_loc.") 
-      and return [];	
+	#chdir($abs_loc); 
+   #   or carp("pdfimages() cannot chdir into $abs_loc.") 
+   #   and return [];	
+   # TODO this is very freaking weird.. sometimes if you call the app pdfimages with full path, it bonks out
 
-   my @args=($WHICH_PDFIMAGES, $abs_pdf,$filename_only);
-   debug("args [@args]");   
-	system(@args) == 0
-		or croak("system [@args] bad.. $?");	
+   
+   #my @args=($WHICH_PDFIMAGES, $abs_pdf, "$abs_loc/$filename_only");
+   #my @args=('pdfimages', $abs_pdf, "$abs_loc/$filename_only");
+   #debug("args [bin absin namespace] [@args]");   
+	#system(@args) == 0
 
-   if($_copied){
-      unlink $abs_pdf;
+
+   my $cmd = "pdfimages '$abs_pdf' '$abs_loc/$filename_only'";   
+
+   debug('cwd is '.cwd().", $cmd");
+
+   system($cmd) == 0   
+      or croak("bad args for pdfimages [$cmd]");
+	#	or croak("system [@args] bad.. $?");	 # what was the problem passing an array of args?? I think 
+   # there was something funny about it...
+
+
+   if( @TRASH and scalar @TRASH){
+      debug("had copied, deleting @TRASH");
+      unlink @TRASH;
    }
 
-	opendir(DIR, $abs_loc) or die($!);
-	my @pagefiles = map { "$abs_loc/$_" } sort grep { /$filename_only.+\.p.m$/i } readdir DIR;
+	opendir(DIR, $abs_loc) 
+      or croak("can't open '$abs_loc' dir, $!");
+   my @ls = readdir DIR;
+   debug("ls is @ls");
+   my @pagefiles = map { "$abs_loc/$_" } sort grep { /$filename_only.+\.p.m$/i } @ls;
+
+	#my @pagefiles = map { "$abs_loc/$_" } sort grep { /$filename_only.+\.p.m$/i } readdir DIR;
 	closedir DIR;
 
-   chdir ($cwd);
+   #chdir ($cwd);
    #	chdir($cwd); # go back to same place we started ??
 
 	unless(scalar @pagefiles){
-		carp("pdfimages() no output from pdfimages for [$abs_pdf]? [$abs_loc]");
+		errstr( __PACKAGE__."::pdfimages() says, no output from pdfimages for [$abs_pdf]?\n[abs loc is: $abs_loc]");
 		return [];
 	}
 
-
+   
 
    if($PDF::GetImages::FORCE_JPG){
-      debug("FORCE_JPG is on");
+      debug("FORCE_JPG is on, converting to jpegs..");
       @pagefiles = _convert_all_to_jpg(@pagefiles);
    }
 	
@@ -111,8 +132,10 @@ sub _convert_to_jpg {
    my $_abs = shift;
    my $_out = $_abs;
    $_out=~s/\.\w{1,5}$/\.jpg/ 
-      or warn("cant match etx on $_abs") and return;
-   require File::Which;
+      or warn("cant match ext on '$_abs'") and return;
+
+
+   
    system($WHICH_CONVERT, $_abs, $_out) ==0 or  die($?);
    unlink $_abs;
    debug(" converted to $_out");
@@ -122,81 +145,4 @@ sub _convert_to_jpg {
 
 1;
 
-__END__
-
-=pod
-
-=head1 NAME
-
-PDF::GetImages - get images from pdf document
-
-=head1 SYNOPSIS
-
-	use PDF::GetImages 'pdfimages';
-
-	my $images = pdfimages('/abs/path/tofile.pdf');
-
-=head1 DESCRIPTION
-
-Get images out of a pdf document. 
-This code makes use of pdfimages which is part
-of xpdf. 
-In case CAM::PDF scripts don't work for you, you may want to try using 
-this to extract images from PDF documents.
-See L<DEPENDENCIES AND REQUIREMENTS>
-
-=head1 pdfimages()
-
-argument is abs path to pdf doc
-optional argument is a dir to which to send images extracted
-returns abs paths to images extracted
-images are extracted by default to same dir pdf is in
-
-If this is not a pdf, the file does not exist, or no images 
-are extracted, warns and returns empty array ref []
-
-=head1 DEBUG
-
-   $PDF::GetImages::DEBUG = 1;
-
-=head1 FORCE_JPG
-
-By default pdfimages will spit out pbm or ppm image format files which are huge and unruly.
-If you want to make sure the images output are jpg..
-
-   $PDF::GetImages::FORCE_JPG= 1 ;
-
-You must have imagemagick convert installed for this to work.
-
-=head1 DEPENDENCIES AND REQUIREMENTS
-
-This module requires Unix family operating system to be installed. 
-You must have 'xpdf' package and 'ImageMagick' convert installed.
-Presently we are using cli 'pdfgetimages'. 
-You must have xpdf installed on your system.
-
-=head1 AUTHOR
-
-Leo Charre leocharre at cpan dot org
-
-=head1 COPYRIGHT
-
-Copyright (c) 2008 Leo Charre. All rights reserved.
-
-=head1 LICENSE
-
-This package is free software; you can redistribute it and/or modify it under the same terms as Perl itself, i.e., under the terms of the "Artistic License" or the "GNU General Public License".
-
-=head1 DISCLAIMER
-
-This package is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-See the "GNU General Public License" for more details.
-
-=head1 SEE ALSO
-
-http://www.imagemagick.org/, 
-xpdf, 
-L<CAM::PDF>
-
-=cut
+# doc moved to lib/PDF/GetImages.pod
